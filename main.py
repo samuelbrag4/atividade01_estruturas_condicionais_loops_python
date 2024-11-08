d5cb300e7d6886a3b1355c5c99c3f4e99f5c7b42
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-08 01:30:11 -0300

    feat: creation of the countdown

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 # Solicita ao usuário que ele insira um número
 numero = float(input("Insira um numero:"))
-# OBS: O float foi utilizada para que o usuário insira somente números decimais
+# OBS: O float foi utilizado para que o usuário insira números inteiros ou decimais
 
 # Verifica se o número é positivo
 if numero > 0:
@@ -19,3 +19,28 @@ elif numero < 0:
 # Verifica se o número é zero
 else :
     print("O numero inserido é zero.")
+    
+    
+# ----- Objetivo 2 -----
+
+# Desenvolvimento de um programa que faça uma contagem regressiva a patir de um número fornecido pelo usuário, de modo que a contagem termine em zero.
+
+# -- Código --
+
+# Solicita ao usuário que ele insira um número
+numero_2 = int(input("Insira um numero:"))    
+# OBS: O int foi utilizado para que o usuário insira somente números inteiros
+
+# Realiação da contagem regressiva 
+for i in range(numero_2, -1, -1):
+    print(i)
+    
+# Explicações:
+
+# 1. O range é uma função que faz uma sequência de números
+# 2. O primeiro argumento é o número inicial da sequência
+# 3. O segundo argumento é o número final da sequência
+# 4. O terceiro argumento é o passo da sequência
+# 5. O -1 no terceiro argumento faz com que a sequência seja decrescente
+# 6. O -1 no segundo argumento faz com que a sequência vá até o número 0
+# 7. O print(i) exibe cada número da sequência
